5faf05a0864d3c2d35b1aa87ae89cef04c076552
utkarshchhabra492020@gmail.com <utkarshchhabra492020@gmail.com>
2026-03-09 14:51:02 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,8 @@
-public class PalindromeCheckerApp {
+    public class PalindromeCheckerApp
+    {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To the Palindrome Checker Management System \n Version : 1.0 \n System initialized successfully.");
+
+    }
 }
