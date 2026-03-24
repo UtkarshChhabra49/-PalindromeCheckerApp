f5ee2bbcd093a906538e23b3474b4fe532209e9f
Utkarsh Chhabra <utkarshchhabra492020@gmail.com>
2026-03-24 15:34:35 +0530

    UC12

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,41 +1,76 @@
+import java.util.Stack;
+import java.util.Deque;
+import java.util.LinkedList;
 
-    public class PalindromeCheckerApp
-    {
+// Strategy Interface
+interface PalindromeStrategy {
+    boolean checkPalindrome(String str);
+}
 
+// Stack-based Strategy
+class StackStrategy implements PalindromeStrategy {
 
-            public static void main(String[] args) {
+    public boolean checkPalindrome(String str) {
+        Stack<Character> stack = new Stack<>();
 
-                String str = "Madam In Eden Im Adam";
-
-                // Normalize string: remove spaces and convert to lowercase
-                String normalized = str.replaceAll("\\s+", "").toLowerCase();
-
-                boolean isPalindrome = true;
-                int start = 0;
-                int end = normalized.length() - 1;
-
-                // Compare characters
-                while (start < end) {
-                    if (normalized.charAt(start) != normalized.charAt(end)) {
-                        isPalindrome = false;
-                        break;
-                    }
-                    start++;
-                    end--;
-                }
-
-                // Print result
-                if (isPalindrome) {
-                    System.out.println("\"" + str + "\" is a Palindrome");
-                } else {
-                    System.out.println("\"" + str + "\" is not a Palindrome");
-                }
-            }
+        for (char c : str.toCharArray()) {
+            stack.push(c);
         }
 
+        for (char c : str.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
+// Deque-based Strategy
+class DequeStrategy implements PalindromeStrategy {
 
+    public boolean checkPalindrome(String str) {
+        Deque<Character> deque = new LinkedList<>();
 
+        for (char c : str.toCharArray()) {
+            deque.addLast(c);
+        }
 
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
+// Context Class
+class PalindromeCheckerApp {
 
+    private PalindromeStrategy strategy;
+
+    public void setStrategy(PalindromeStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public boolean check(String str) {
+        return strategy.checkPalindrome(str);
+    }
+}
+
+// ✅ Main Class (FIXED)
+class Main {
+    public static void main(String[] args) {
+
+        String word = "level";
+
+        PalindromeCheckerApp checker = new PalindromeCheckerApp();
+
+        checker.setStrategy(new StackStrategy());
+        System.out.println("Using Stack: " + checker.check(word));
+
+        checker.setStrategy(new DequeStrategy());
+        System.out.println("Using Deque: " + checker.check(word));
+    }
+}
